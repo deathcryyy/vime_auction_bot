e0fb46ebb4c289d061c23ebf56c51df3986b5753
deathcryyy <155384299+deathcryyy@users.noreply.github.com>
2026-02-14 15:49:24 +0400

    bot.py

--- bot.py
+++ bot.py
@@ -5,7 +5,7 @@ from pathlib import Path
 from config import API_URL, CHECK_INTERVAL, ALERT_BEFORE_END_HOURS, WATCHED_NICKS
 
 # Настройки для автоотправки изображения
-IMAGE_SEND_INTERVAL = 3600  # 1 час в секундах
+IMAGE_SEND_INTERVAL = 60  # 1 час в секундах
 IMAGE_PATH = Path(__file__).parent / "image.jpg"  # Путь к изображению
 
 known_auctions = set()
@@ -233,3 +233,4 @@ if __name__ == "__main__":
         )
     
     asyncio.run(main())
+
